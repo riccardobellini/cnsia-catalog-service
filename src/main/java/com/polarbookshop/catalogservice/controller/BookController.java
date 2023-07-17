d3a0cde59cda76e7a808d034655b51b5f181cf50
Riccardo Bellini <riccardo.bellini@rold.com>
2023-07-17 16:41:38 +0200

    Add logs

--- src/main/java/com/polarbookshop/catalogservice/controller/BookController.java
+++ src/main/java/com/polarbookshop/catalogservice/controller/BookController.java
@@ -2,13 +2,14 @@ package com.polarbookshop.catalogservice.controller;
 
 import com.polarbookshop.catalogservice.domain.Book;
 import com.polarbookshop.catalogservice.domain.BookService;
-import com.polarbookshop.catalogservice.dto.BookListDto;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("books")
+@Slf4j
 public class BookController {
 
     private final BookService bookService;
@@ -18,29 +19,34 @@ public class BookController {
     }
 
     @GetMapping
-    public BookListDto getAll() {
-        return new BookListDto(bookService.viewList());
+    public Iterable<Book> getAll() {
+        log.info("Fetching the list of books in the catalog");
+        return bookService.viewList();
     }
 
     @GetMapping("{isbn}")
     public Book getByIsbn(@PathVariable("isbn") String isbn) {
+        log.info("Fetching the book with ISBN {} from the catalog", isbn);
         return bookService.viewDetails(isbn);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book create(@Valid @RequestBody Book book) {
+        log.info("Adding a new book to the catalog with ISBN {}", book.isbn());
         return bookService.addToCatalog(book);
     }
 
     @DeleteMapping("{isbn}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable("isbn") String isbn) {
+        log.info("Deleting book with ISBN {}", isbn);
         bookService.removeFromCatalog(isbn);
     }
 
     @PutMapping("{isbn}")
     public Book update(@PathVariable("isbn") String isbn, @Valid @RequestBody Book book) {
+        log.info("Updating book with ISBN {}", isbn);
         return bookService.editDetails(isbn, book);
     }
 }
